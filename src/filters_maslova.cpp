ce7e190c9617a42b72d1798c31343e3883594bcb
konovalov25 <ss2015@ws-2k-114-09n.ccam.cmc.unn.net>
2015-08-18 13:48:13 +0300

    filter2d

--- src/filters_maslova.cpp
+++ src/filters_maslova.cpp
@@ -3,25 +3,21 @@
 class FiltersMaslova : public Filters{
 	virtual void boxFilter(const Matrix &src, Matrix &dst, const int kSize = 3) {};
 	virtual void filter2d(const Matrix &src, Matrix &dst, const Matrix &kernel){
-		const int R=src.rows(), C=src.cols(), RF=kernel.rows(), CF=kernel.cols();
-		Matrix matr(R+RF, C+CF);
-		matr.Zeros();
-		//copy
-		for (int x=0; x<R; x++)
-			for (int y=0; y<C; y++){
-				matr[x][y]=src[x][y];
-			};
+		const int R=src.rows(), C=src.cols(), RF=kernel.rows()%2, CF=kernel.cols()%2;
 		
 		//filter
 		dst = Matrix(R,C);
 		dst.Zeros();
 		for (int x=0; x<R; x++)
 			for (int y=0; y<C; y++)
-				for (int i=0; (i<RF)&&(i<(R-x));i++)
-					for (int j=0; (j<CF)&&(j<(C-y));j++){
-						dst[x][y]+=kernel[i][j]*matr[x+i][y+j];
+				for (int i=-RF; i<=RF;i++)
+				if(((x+i)>=0)&&((x+i)<R))
+					for (int j=-CF; j<=CF;j++)
+					if (((y+j)>=0)&&((y+j)<C)){
+						std::cout<<"I'm here "<<x<<" "<<i<<" "<<y<<" "<<j<<std::endl;
+						dst[x][y]+=kernel[i+RF][j+CF]*src[x+i][y+j];
+						
 					}
-
 	};
 	virtual void median(const Matrix &src, Matrix &dst, const int kSize = 3) {};
 	virtual void SobelOx(const Matrix &src, Matrix &dst) {};
