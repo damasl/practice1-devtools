f5629545462fc6d776edf1f82074edbd6818f97d
maslova <ss2015@ws-2k-114-09n.ccam.cmc.unn.net>
2015-08-18 14:48:51 +0300

    box filter

--- src/filters_maslova.cpp
+++ src/filters_maslova.cpp
@@ -1,10 +1,33 @@
 #include "filters.hpp"
 
+int chooseBoxIndex(int xi, int max)
+{
+	while(true)
+	{
+		if (xi>max) xi = max+max-xi;
+		else if (xi<0) xi *=-1;
+		else break;
+	}
+	return xi;
+}
+
+
 class FiltersMaslova : public Filters{
-	virtual void boxFilter(const Matrix &src, Matrix &dst, const int kSize = 3) {};
+	virtual void boxFilter(const Matrix &src, Matrix &dst, const int kSize = 3) {
+		const int R=src.rows(), C=src.cols(), k2=kSize*kSize, k = kSize/2;
+		//filter
+		dst = Matrix(R,C);
+		dst.Zeros();
+		for (int x=0; x<R; x++)
+			for (int y=0; y<C; y++){
+				for (int i = -k; i<=k; i++)
+					for (int j =-k; j<=k; j++)
+						dst[x][y]+=src[chooseBoxIndex(x+i, R-1)][chooseBoxIndex(y+j, C-1)];
+				dst[x][y]/=k2;
+			}
+	};
 	virtual void filter2d(const Matrix &src, Matrix &dst, const Matrix &kernel){
-		const int R=src.rows(), C=src.cols(), RF=kernel.rows()%2, CF=kernel.cols()%2;
-		
+		const int R=src.rows(), C=src.cols(), RF=kernel.rows()/2, CF=kernel.cols()/2;
 		//filter
 		dst = Matrix(R,C);
 		dst.Zeros();
@@ -14,9 +37,7 @@ class FiltersMaslova : public Filters{
 				if(((x+i)>=0)&&((x+i)<R))
 					for (int j=-CF; j<=CF;j++)
 					if (((y+j)>=0)&&((y+j)<C)){
-						std::cout<<"I'm here "<<x<<" "<<i<<" "<<y<<" "<<j<<std::endl;
-						dst[x][y]+=kernel[i+RF][j+CF]*src[x+i][y+j];
-						
+						dst[x][y]+=kernel[i+RF][j+CF]*src[x+i][y+j];	
 					}
 	};
 	virtual void median(const Matrix &src, Matrix &dst, const int kSize = 3) {};
